3511069cac59231e859b902d660f565150316159
clickonuser <clickonuser@gmail.com>
2024-12-19 16:19:41 +0300

    Create Entities

---
+++ app/src/main/java/com/example/shoplistcompose/data/ShoppingListName.kt
@@ -0,0 +1,14 @@
+package com.example.shoplistcompose.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shop_list_name")
+data class ShoppingListName(
+    @PrimaryKey
+    val id: Int? = null,
+    val name: String,
+    val time: String,
+    val allItemsCount: Int,
+    val allSelectedItemsCount: Int
+)
