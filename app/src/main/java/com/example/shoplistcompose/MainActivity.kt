ae8c9b34a612f5bbd44f81b968616af780350a54
clickonuser <clickonuser@gmail.com>
2024-12-20 16:29:56 +0300

    Integrate Hilt

--- app/src/main/java/com/example/shoplistcompose/MainActivity.kt
+++ app/src/main/java/com/example/shoplistcompose/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.shoplistcompose.ui.theme.ShopListComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
