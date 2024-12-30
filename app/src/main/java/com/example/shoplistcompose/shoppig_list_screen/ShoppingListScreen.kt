ec586e44e72dce21a02e06222ef6a7897143aae8
clickonuser <clickonuser@gmail.com>
2024-12-30 11:07:43 +0300

    add ShoppingListViewModel to ShoppingListScreen compose using hilt

--- app/src/main/java/com/example/shoplistcompose/shoppig_list_screen/ShoppingListScreen.kt
+++ app/src/main/java/com/example/shoplistcompose/shoppig_list_screen/ShoppingListScreen.kt
@@ -6,9 +6,12 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun ShoppingListScreen() {
+fun ShoppingListScreen(
+    viewModel: ShoppingListViewModel = hiltViewModel()
+) {
     Text(
         text = "Shopping list screen",
         modifier = Modifier
