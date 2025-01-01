95cdf64cec17efdf82867b9190a97afaee2dffe4
clickonuser <clickonuser@gmail.com>
2025-01-01 21:09:28 +0300

    ShoppingListScreen show list

--- app/src/main/java/com/example/shoplistcompose/shoppig_list_screen/ShoppingListScreen.kt
+++ app/src/main/java/com/example/shoplistcompose/shoppig_list_screen/ShoppingListScreen.kt
@@ -1,22 +1,28 @@
 package com.example.shoplistcompose.shoppig_list_screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.shoplistcompose.ui.theme.GrayLight
 
 @Composable
 fun ShoppingListScreen(
     viewModel: ShoppingListViewModel = hiltViewModel()
 ) {
-    Text(
-        text = "Shopping list screen",
+    val itemsList = viewModel.list.collectAsState(initial = emptyList())
+
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .wrapContentWidth()
-            .wrapContentHeight()
-    )
+            .background(GrayLight),
+    ) {
+        items(itemsList.value) {
+            UiShoppingListItem()
+        }
+    }
 }
