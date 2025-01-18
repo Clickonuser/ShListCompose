12b40993679a867a68c5fe3752d082d3c22a7084
clickonuser <clickonuser@gmail.com>
2025-01-18 16:38:40 +0300

    Create UiAddItem

---
+++ app/src/main/java/com/example/shoplistcompose/add_item_screen/UiAddItem.kt
@@ -0,0 +1,64 @@
+package com.example.shoplistcompose.add_item_screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.shoplistcompose.R
+import com.example.shoplistcompose.data.AddItem
+
+@Composable
+fun UiAddItem(item: AddItem, onEvent: (AddItemEvent) -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(3.dp)
+            .clickable {
+                onEvent(AddItemEvent.OnShowEditDialog(item))
+            },
+        shape = RoundedCornerShape(5.dp) // changes may come later
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White), // changes may come later)
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 10.dp),
+                text = item.name,
+                fontSize = 12.sp
+            )
+            Checkbox(
+                checked = item.isCheck,
+                onCheckedChange = { isChecked ->
+                    onEvent(AddItemEvent.OnCheckedChange(item.copy(isCheck = isChecked)))
+                }
+            )
+            IconButton(onClick = {
+                onEvent(AddItemEvent.OnDelete(item))
+            }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.delete_icon),
+                    contentDescription = "Delete"
+                )
+            }
+        }
+    }
+}
