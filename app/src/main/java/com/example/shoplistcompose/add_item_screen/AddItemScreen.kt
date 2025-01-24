a5d08becb1f7c2a33ddc2b64367df815af9a8665
clickonuser <clickonuser@gmail.com>
2025-01-24 16:22:32 +0300

    Add an empty stub

--- app/src/main/java/com/example/shoplistcompose/add_item_screen/AddItemScreen.kt
+++ app/src/main/java/com/example/shoplistcompose/add_item_screen/AddItemScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -29,6 +30,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -124,5 +126,15 @@ fun AddItemScreen(
             }
         }
         MainDialog(dialogController = viewModel)
+        if(itemsList?.value?.isEmpty() == true) {
+            Text(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentHeight(),
+                text = "Empty",
+                fontSize = 25.sp,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
--- app/src/main/java/com/example/shoplistcompose/shoppig_list_screen/ShoppingListScreen.kt
+++ app/src/main/java/com/example/shoplistcompose/shoppig_list_screen/ShoppingListScreen.kt
@@ -3,13 +3,17 @@ package com.example.shoplistcompose.shoppig_list_screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.shoplistcompose.dialog.MainDialog
 import com.example.shoplistcompose.ui.theme.GrayLight
@@ -44,4 +48,14 @@ fun ShoppingListScreen(
         }
     }
     MainDialog(dialogController = viewModel)
+    if(itemsList.value.isEmpty()) {
+        Text(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentHeight(),
+            text = "Empty",
+            fontSize = 25.sp,
+            textAlign = TextAlign.Center
+        )
+    }
 }
