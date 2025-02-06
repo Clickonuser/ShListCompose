3c0b901490be9fb3a11eeaeb830ffeaf52ed9cde
clickonuser <clickonuser@gmail.com>
2025-02-06 15:57:44 +0300

    Create NoteListEvent and NoteListViewModel

---
+++ app/src/main/java/com/example/shoplistcompose/note_list_screen/NoteListEvent.kt
@@ -0,0 +1,8 @@
+package com.example.shoplistcompose.note_list_screen
+
+import com.example.shoplistcompose.data.NoteItem
+
+sealed class NoteListEvent {
+    data class OnShowDeleteDialog(val item: NoteItem) : NoteListEvent()
+    data class OnItemClick(val route: String) : NoteListEvent()
+}
