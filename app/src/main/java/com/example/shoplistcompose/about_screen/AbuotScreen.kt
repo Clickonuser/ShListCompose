1db84d82073400934594645aa0b27a215ad3b552
clickonuser <clickonuser@gmail.com>
2025-02-08 00:20:19 +0300

    AboutScreen design

--- app/src/main/java/com/example/shoplistcompose/about_screen/AbuotScreen.kt
+++ app/src/main/java/com/example/shoplistcompose/about_screen/AbuotScreen.kt
@@ -1,19 +1,57 @@
 package com.example.shoplistcompose.about_screen
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.shoplistcompose.R
+import com.example.shoplistcompose.ui.theme.BlueLight
 
+@Preview(showBackground = true)
 @Composable
 fun AboutScreen() {
-    Text(
-        text = "About screen",
-        modifier = Modifier
-            .fillMaxSize()
-            .wrapContentWidth()
-            .wrapContentHeight()
-    )
+    val uriHandler = LocalUriHandler.current
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.fake_logo),
+            contentDescription = "Logo",
+            modifier = Modifier.size(100.dp),
+            tint = BlueLight
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            text = "This is a test description \n" +
+                    "Version - 1.0.3 \n" +
+                    "To get more information: \n",
+            textAlign = TextAlign.Center
+            )
+        Text(
+            modifier = Modifier
+                .padding(top = 10.dp)
+                .clickable {
+                    uriHandler.openUri("https://www.google.com/")
+                }
+            ,
+            text = "Google",
+            color = BlueLight
+        )
+    }
 }
