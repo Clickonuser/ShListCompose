4195b1d782114e7877e8b48cd3d6d21e36e2d76e
clickonuser <clickonuser@gmail.com>
2025-02-08 15:59:32 +0300

    Add SettingScreen UI and ViewModel

---
+++ app/src/main/java/com/example/shoplistcompose/settings_screen/SettingsViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.shoplistcompose.settings_screen
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingsViewModel @Inject constructor(): ViewModel() {
+
+    val colorItemListState = mutableStateOf<List<ColorItem>>(emptyList())
+
+    init {
+        viewModelScope.launch {
+            val tempColorItemList = ArrayList<ColorItem>()
+            ColorUtils.colorList.forEach { color ->
+                tempColorItemList.add(
+                    ColorItem(
+                        color = color,
+                        isSelect = false
+                    )
+                )
+            }
+            colorItemListState.value = tempColorItemList
+        }
+    }
+}
